8f76d42aa8adf39ec0e8c2ca048e8da139489502
sbohmann <github@yeoman.at>
2024-12-13 17:32:08 +0100

    Day 10 A

---
+++ src/main/kotlin/day10/day10.kt
@@ -0,0 +1,92 @@
+package day10
+
+import java.io.File
+
+fun main() {
+    val map = readMapData()
+
+    println("A: ${A(map).result}")
+}
+
+class A(val map: Map) {
+    val result: Int
+
+    init {
+        result = map.fields()
+            .filter { it.elevation == 0 }
+            .fold(0) { sum, field ->
+                sum + endPositionsReachable(field)
+            }
+    }
+
+    fun endPositionsReachable(field: Field): Int {
+        var state = setOf(field)
+        for (elevation in field.elevation..<9) {
+            val reachableEndFields = state.flatMap { HashSet(map.neighboringPositionsElevatedByOne(it)) }
+            state = HashSet(reachableEndFields)
+        }
+        return state.size
+    }
+}
+
+private fun readMapData(): Map {
+    val mapData = File("input")
+        .readLines()
+        .withIndex()
+        .map { (y, line) -> parseLine(y, line) }
+    val width = mapData.first().size
+    val height = mapData.size
+    return Map(width, height, mapData)
+}
+
+fun parseLine(y: Int, line: String): List<Field> {
+    return line.withIndex()
+        .map { (x, c) ->
+            val elevation = c - '0'
+            Field(
+                Position(x, y), elevation
+            )
+        }
+}
+
+class Map(val width: Int, val height: Int, private val data: List<List<Field>>) {
+    fun fields(): List<Field> {
+        return data.flatten()
+    }
+
+    fun neighboringPositionsElevatedByOne(field: Field): List<Field> {
+        return neighboringFields(field.position)
+            .filter { it.elevation == field.elevation + 1 }
+    }
+
+    fun neighboringFields(position: Position): List<Field> {
+        return Direction.entries
+            .map { Position(position.x + it.dx, position.y + it.dy) }
+            .filter(this::inside)
+            .map(this::fieldForPosition)
+    }
+
+    private fun inside(position: Position): Boolean {
+        return inside(position.x, position.y)
+    }
+
+    private fun inside(x: Int, y: Int): Boolean {
+        return x >= 0 && y >= 0 &&
+                x < width && y < height
+    }
+
+    private fun fieldForPosition(position: Position): Field {
+        return data[position.y][position.x]
+    }
+}
+
+class Field(val position: Position, val elevation: Int)
+
+data class Position(val x: Int, val y: Int)
+
+enum class Direction(val dx: Int, val dy: Int) {
+    Up(0, -1),
+    Down(0, 1),
+    Left(-1, 0),
+    Right(1, 0)
+}
