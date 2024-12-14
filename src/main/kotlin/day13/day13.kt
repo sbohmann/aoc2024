29809cfbbc2883cf31347b642f92ab2f7b25800c
sbohmann <github@yeoman.at>
2024-12-14 21:46:36 +0100

    Day 13

--- src/main/kotlin/day13/day13.kt
+++ src/main/kotlin/day13/day13.kt
@@ -16,25 +16,41 @@ fun main() {
         .split(doubleNewline)
         .map { parseSetup(it) }
     println("A: ${solveA(setups)}")
+    println("B: ${solveB(setups)}")
 }
 
 fun parseSetup(input: String): Setup {
     val match = setupRegex.matchEntire(input)!!
     return Setup(
-        Step(match.groups[1]!!.value.toInt(), match.groups[2]!!.value.toInt()),
-        Step(match.groups[3]!!.value.toInt(), match.groups[4]!!.value.toInt()),
-        Position(match.groups[5]!!.value.toInt(), match.groups[6]!!.value.toInt())
+        Step(match.groups[1]!!.value.toLong(), match.groups[2]!!.value.toLong()),
+        Step(match.groups[3]!!.value.toLong(), match.groups[4]!!.value.toLong()),
+        Position(match.groups[5]!!.value.toLong(), match.groups[6]!!.value.toLong())
     )
 }
 
-fun solveA(setups: List<Setup>): Int {
+fun solveA(setups: List<Setup>): Long {
     return setups
         .fold(0) { sum, next -> sum + minimumTokensForSetup(next) }
 }
 
-fun minimumTokensForSetup(setup: Setup): Int {
+fun solveB(setups: List<Setup>): Long {
+    return setups
+        .map { setup ->
+            setup.copy(prize = prizeAdjustedForBSolution(setup.prize))
+        }
+        .fold(0) { sum, next -> sum + minimumTokensForSetup(next) }
+}
+
+fun prizeAdjustedForBSolution(prize: Position): Position {
+    return Position(
+        prize.x + 10000000000000L,
+        prize.y + 10000000000000L
+    )
+}
+
+fun minimumTokensForSetup(setup: Setup): Long {
     var aButtonCount = 0
-    val results = mutableListOf<Int>()
+    val results = mutableListOf<Long>()
     while (true) {
         val xOffset = aButtonCount * setup.a.dx
         val yOffset = aButtonCount * setup.a.dy
@@ -50,8 +66,8 @@ fun minimumTokensForSetup(setup: Setup): Int {
     return results.minOrNull() ?: 0
 }
 
-fun findBButtonCount(setup: Setup, xOffset: Int, yOffset: Int): Int? {
-    var bButtonCount = 0
+fun findBButtonCount(setup: Setup, xOffset: Long, yOffset: Long): Long? {
+    var bButtonCount = 0L
     while (true) {
         val dx = bButtonCount * setup.b.dx
         val dy = bButtonCount * setup.b.dy
@@ -69,6 +85,6 @@ fun findBButtonCount(setup: Setup, xOffset: Int, yOffset: Int): Int? {
 
 data class Setup(val a: Step, val b: Step, val prize: Position)
 
-data class Position(val x: Int, val y: Int)
+data class Position(val x: Long, val y: Long)
 
-data class Step(val dx: Int, val dy: Int)
+data class Step(val dx: Long, val dy: Long)
