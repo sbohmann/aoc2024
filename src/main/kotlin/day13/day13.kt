fa2bf245d9cfb359d4c33e5fe6e4f118d0da8958
sbohmann <github@yeoman.at>
2024-12-14 21:33:49 +0100

    Day 13

--- src/main/kotlin/day13/day13.kt
+++ src/main/kotlin/day13/day13.kt
@@ -23,7 +23,7 @@ fun parseSetup(input: String): Setup {
     return Setup(
         Step(match.groups[1]!!.value.toInt(), match.groups[2]!!.value.toInt()),
         Step(match.groups[3]!!.value.toInt(), match.groups[4]!!.value.toInt()),
-        Position(match.groups[3]!!.value.toInt(), match.groups[4]!!.value.toInt())
+        Position(match.groups[5]!!.value.toInt(), match.groups[6]!!.value.toInt())
     )
 }
 
@@ -33,7 +33,38 @@ fun solveA(setups: List<Setup>): Int {
 }
 
 fun minimumTokensForSetup(setup: Setup): Int {
-    return 0
+    var aButtonCount = 0
+    val results = mutableListOf<Int>()
+    while (true) {
+        val xOffset = aButtonCount * setup.a.dx
+        val yOffset = aButtonCount * setup.a.dy
+        if (xOffset > setup.prize.x || yOffset > setup.prize.y) {
+            break
+        }
+        val bButtonCount = findBButtonCount(setup, xOffset, yOffset)
+        if (bButtonCount != null) {
+            results.add(3 * aButtonCount + bButtonCount)
+        }
+        ++aButtonCount
+    }
+    return results.minOrNull() ?: 0
+}
+
+fun findBButtonCount(setup: Setup, xOffset: Int, yOffset: Int): Int? {
+    var bButtonCount = 0
+    while (true) {
+        val dx = bButtonCount * setup.b.dx
+        val dy = bButtonCount * setup.b.dy
+        val x = xOffset + dx
+        val y = yOffset + dy
+        if (x == setup.prize.x && y == setup.prize.y) {
+            return bButtonCount
+        }
+        if (x > setup.prize.x || y > setup.prize.y) {
+            return null
+        }
+        ++bButtonCount
+    }
 }
 
 data class Setup(val a: Step, val b: Step, val prize: Position)
